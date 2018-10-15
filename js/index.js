e545d13450437010ae23ff4e473e8b8c760b37b8
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-15 20:05:49 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -15,3 +15,11 @@ input.addEventListener('keydown', function(e) {
     console.log(e.which)
   }
 })
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
